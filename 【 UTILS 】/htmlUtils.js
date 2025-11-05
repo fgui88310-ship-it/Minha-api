ebb59989f51d99fcb407d0d07c1ae8ae7a4297ce
Guilherme <guilherme993344@gmail.com>
2025-11-05 17:23:13 -0300

    UPGRADE CANVAS 2

--- 【 UTILS 】/htmlUtils.js
+++ 【 UTILS 】/htmlUtils.js
@@ -15,24 +15,3 @@ import axios from 'axios';
 
 export const USER_AGENT =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
-
-// 🔸 Faz uma requisição com cabeçalhos padrão
-export async function fetchHTML(url) {
-  const { data } = await axios.get(url, {
-    headers: { 'User-Agent': USER_AGENT },
-    timeout: 10000,
-  });
-  return data;
-}
-
-// 🔸 Resolve URLs com redirecionamentos (ex: shortlinks da Globo)
-export async function resolverUrlRedirecionada(url) {
-  try {
-    if (url.startsWith('//')) url = 'https:' + url;
-    const response = await axios.get(url, { maxRedirects: 5 });
-    return response.request.res.responseUrl || url;
-  } catch (err) {
-    console.error('[HTTP UTILS][REDIRECT]', err.message);
-    return url;
-  }
-}
--- 【 UTILS 】/httpUtils.js
+++ 【 UTILS 】/httpUtils.js
@@ -9,3 +9,23 @@ export async function fetchHTML(url) {
   });
   return data;
 }
+// 🔸 Faz uma requisição com cabeçalhos padrão
+export async function fetchHTML(url) {
+  const { data } = await axios.get(url, {
+    headers: { 'User-Agent': USER_AGENT },
+    timeout: 10000,
+  });
+  return data;
+}
+
+// 🔸 Resolve URLs com redirecionamentos (ex: shortlinks da Globo)
+export async function resolverUrlRedirecionada(url) {
+  try {
+    if (url.startsWith('//')) url = 'https:' + url;
+    const response = await axios.get(url, { maxRedirects: 5 });
+    return response.request.res.responseUrl || url;
+  } catch (err) {
+    console.error('[HTTP UTILS][REDIRECT]', err.message);
+    return url;
+  }
+}
